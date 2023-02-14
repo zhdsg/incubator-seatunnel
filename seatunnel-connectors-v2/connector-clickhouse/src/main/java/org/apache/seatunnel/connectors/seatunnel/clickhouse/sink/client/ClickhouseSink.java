7aeeecb4f0b8ff8e1d45b0b756d8253be833ec79
zhdsg <704448732@qq.com>
2023-02-15 02:10:18 +0800

    增加筛选字段逻辑

--- seatunnel-connectors-v2/connector-clickhouse/src/main/java/org/apache/seatunnel/connectors/seatunnel/clickhouse/sink/client/ClickhouseSink.java
+++ seatunnel-connectors-v2/connector-clickhouse/src/main/java/org/apache/seatunnel/connectors/seatunnel/clickhouse/sink/client/ClickhouseSink.java
@@ -241,22 +241,23 @@ public class ClickhouseSink
     @Override
     public void setTypeInfo(SeaTunnelRowType seaTunnelRowType) {
 
-        Map<String, String> tableSchema = option.getTableSchema();
-        String[] newFieldNames =new String[tableSchema.size()];
-        SeaTunnelDataType<?>[] newFieldTypes = new  SeaTunnelDataType<?>[tableSchema.size()];
-        String[] fieldNames = seaTunnelRowType.getFieldNames();
-        SeaTunnelDataType<?>[] fieldTypes = seaTunnelRowType.getFieldTypes();
-        for (int i =0 ,j=0;i <fieldNames.length;i++){
-            if(tableSchema.keySet().contains(fieldNames[i])){
-                newFieldNames[j]=fieldNames[i];
-                newFieldTypes[j]=fieldTypes[j];
-                j++;
-            }
-        }
-
-        this.option.setSeaTunnelRowType(
-                new SeaTunnelRowType(newFieldNames,newFieldTypes)
-        );
+//        Map<String, String> tableSchema = option.getTableSchema();
+//        String[] newFieldNames =new String[tableSchema.size()];
+//        SeaTunnelDataType<?>[] newFieldTypes = new  SeaTunnelDataType<?>[tableSchema.size()];
+//        String[] fieldNames = seaTunnelRowType.getFieldNames();
+//        SeaTunnelDataType<?>[] fieldTypes = seaTunnelRowType.getFieldTypes();
+//        for (int i =0 ,j=0;i <fieldNames.length;i++){
+//            if(tableSchema.keySet().contains(fieldNames[i])){
+//                newFieldNames[j]=fieldNames[i];
+//                newFieldTypes[j]=fieldTypes[j];
+//                j++;
+//            }
+//        }
+//
+//        this.option.setSeaTunnelRowType(
+//                new SeaTunnelRowType(newFieldNames,newFieldTypes)
+//        );
+        this.option.setSeaTunnelRowType(seaTunnelRowType);
     }
 
     @Override
